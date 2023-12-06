4ab69e440255a39a3594a83dc0880b2145df6330
Potato <105754685+dragon731012@users.noreply.github.com>
2023-12-06 15:13:12 -0600

    Add files via upload

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.tiwhosts.me',
+    bare: "https://geoquiz.gq/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
