3eafa2afa0faf9a20a0d81258965f779e129a64e
Shadow <149085044+Shadowproxynetwork@users.noreply.github.com>
2023-12-07 09:02:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-
+import express from 'express';
+import http from 'node:http';
+import path from 'node:path';
+import cors from 'cors';
+import { createBareServer } from "@tomphttp/bare-server-node";
+import fetch from 'node-fetch';  // Make sure to install 'node-fetch' package
+const app = express();
+const server = http.createServer(app);
+const __dirname = process.cwd();
+const PORT = 8080;
+const bareServer = createBareServer('/outerspace/');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/InterstellarNetwork/Interstellar-Assets/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
+});
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
+});
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
