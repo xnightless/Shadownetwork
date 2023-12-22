0a7ffaaa71a2032a563eeef8a1c2b7048e8345bc
Potato <105754685+dragon731012@users.noreply.github.com>
2023-12-22 17:42:39 -0600

    Update nowgg.js

--- nowgg/nowgg.js
+++ nowgg/nowgg.js
@@ -1,5 +1,5 @@
 var doc = document.getElementById("iframe").contentWindow; 
   
 var script=doc.document.createElement("script");
-script.src=window.location.origin+"/scripts/nowggadblockremove.js";
+script.src=window.location.origin+"/nowgg/bypass.js";
 doc.document.body.appendChild(script);
